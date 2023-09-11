5baea307e9e7d1973ca0ad8d5c56e349cc77a25c
rohit7779 <rohit.thorat@styldod.com>
2023-09-11 17:04:21 +0530

    added download queue handler for reimagine 2.0

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 def get_database():
     from pymongo import MongoClient
 
-    CONNECTION_STRING = "mongodb+srv://dev:magic797BoX@magicboxcluster.l4wqb.mongodb.net/development?retryWrites=true&w=majority"
+    CONNECTION_STRING = "mongodb+srv://stage:wN3YPq29ZqmZg7RU@reimaginehomeai-2-0.t5kbrrx.mongodb.net/stage?retryWrites=true&w=majority"
     client = MongoClient(CONNECTION_STRING)
 
-    return client["development"]
+    return client["stage"]
--- run_denoise_task.py
+++ run_denoise_task.py
@@ -6,6 +6,7 @@ import boto3
 from inference_realesrgan import supre_resolution
 from signal_handler import SignalHandler
 import logging
+from db import get_database
 from slack_logger import SlackHandler, SlackFormatter
 
 sh = SlackHandler(
@@ -18,35 +19,56 @@ logging.getLogger().setLevel(logging.INFO)
 tablename = "reimagine-downloads"
 download_table = boto3.resource("dynamodb").Table(tablename)
 
+db = get_database()
 
-def update_job_status(variant_id, status):
+downloadModel = db['downloads']
+
+def update_job_status(variant_id, status,version):
     update_data = {}
     set_string = ""
     if status == "DONE":
-        set_string = "SET job_status = :status, updated_at = :ut"
-        update_data = {
-            ":status": status,
-            ":ut": round(time.time() * 1000),
-        }
+        if version == "v1":
+            set_string = "SET job_status = :status, updated_at = :ut"
+            update_data = {
+                ":status": status,
+                ":ut": round(time.time() * 1000),
+            }
+        else:
+            update_data = {
+                "job_status" : status,
+                "finishedAt" : round(time.time() * 1000),
+            }
 
     if status == "PROCESSING":
-        set_string = "SET job_status = :status, started_at = :ut"
-        update_data = {
-            ":status": status,
-            ":ut": round(time.time() * 1000),
-        }
+        if version == "v1":
+            set_string = "SET job_status = :status, started_at = :ut"
+            update_data = {
+                ":status": status,
+                ":ut": round(time.time() * 1000),
+            }
+        else:
+            update_data = {
+                "job_status" : status,
+                "startedAt" : round(time.time() * 1000),
+            }
 
-    try:
-        download_table.update_item(
-            Key={"variant_id": variant_id},
-            UpdateExpression=set_string,
-            ConditionExpression="attribute_exists(variant_id)",
-            ExpressionAttributeValues=update_data,
+    if version == "v1":
+        try:
+            download_table.update_item(
+                Key={"variant_id": variant_id},
+                UpdateExpression=set_string,
+                ConditionExpression="attribute_exists(variant_id)",
+                ExpressionAttributeValues=update_data,
+            )
+        except Exception as e:
+            print(e)
+            if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+                logging.error("ConditionalCheckFailedException while updating record")
+    else:
+        downloadModel.find_one_and_update(
+        {'variant_id': variant_id},
+        {'$set': update_data}
         )
-    except Exception as e:
-        print(e)
-        if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
-            logging.error("ConditionalCheckFailedException while updating record")
 
 
 if __name__ == "__main__":
@@ -59,7 +81,12 @@ if __name__ == "__main__":
             job = json.loads(body)
             variant_id = job["variant_id"]
 
-            update_job_status(variant_id, "PROCESSING")
+            version = "v1"
+
+            if "version" in job:
+                version = job["version"]
+
+            update_job_status(variant_id, "PROCESSING", version)
 
             new_timeout = 15
             queueHandler.change(data["ReceiptHandle"], new_timeout)
@@ -70,31 +97,39 @@ if __name__ == "__main__":
 
             if "type" in job:
                 upscale_type = job["type"]
+            
+            
 
             try:
                 startTime = time.time()
                 resp = supre_resolution(job["input_url"], upscale_type)
                 endTime = time.time()
 
-                try:
-                    download_table.update_item(
-                        Key={"variant_id": variant_id},
-                        UpdateExpression="SET output_url = :data",
-                        ConditionExpression="attribute_exists(variant_id)",
-                        ExpressionAttributeValues={":data": resp},
-                    )
-                except Exception as e:
-                    print(e)
-                    if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
-                        logging.error(
-                            "ConditionalCheckFailedException while updating record"
+                if version == "v1":
+                    try:
+                        download_table.update_item(
+                            Key={"variant_id": variant_id},
+                            UpdateExpression="SET output_url = :data",
+                            ConditionExpression="attribute_exists(variant_id)",
+                            ExpressionAttributeValues={":data": resp},
                         )
+                    except Exception as e:
+                        print(e)
+                        if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+                            logging.error(
+                                "ConditionalCheckFailedException while updating record"
+                            )
+                else:
+                    downloadModel.find_one_and_update(
+                        {'variant_id': variant_id},
+                        {'$set': {'output_url': resp}}
+                    )
 
             except Exception as e:
                 logging.error(str(e))
                 logging.error("Image Processing Failed")
                 traceback.print_exc()
-                update_job_status(variant_id, "ERROR")
+                update_job_status(variant_id, "ERROR", version)
 
-            update_job_status(variant_id, "DONE")
+            update_job_status(variant_id, "DONE", version)
             queueHandler.delete(data["ReceiptHandle"])
